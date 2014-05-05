0cd4816066cb34459911f006ae086f745bc849a3
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-05 20:10:09 +0900

    Add koma images

--- static/js/shogi.js
+++ static/js/shogi.js
@@ -9,11 +9,62 @@ var updater = {
     }
 };
 
+var KOMA_TYPES = {
+  "king":["king","king"], //王
+  "rook":["rook","promoted_rook"], //飛車
+  "bishop":["bishop","promoted_bishop"], //角
+  "gold_general":["gold","gold"], //金
+  "silver_general":["silver","promoted_silver"], //銀
+  "knight":["knight","promoted_knight"], //馬
+  "lance":["lance","promoted_lance"], //香車
+  "pawn":["pawn","promoted_pawn"] //歩
+}
+
+var Koma = function(koma_type){
+  if(typeof koma_type == "string"){
+    koma_type = KOMA_TYPES[koma_type]
+  }
+  if(koma_type == undefined){
+    throw "koma_type "+koma_type+" is not defined"
+  }
+  this.koma_img_path = STATIC_URL + "images/" + koma_type[0] + ".png";
+  this.koma = new Image();
+  this.koma.width = 80
+  this.koma.src = this.koma_img_path;
+
+  this.promoted_koma_img_path = STATIC_URL + "images/" + koma_type[1] + ".png";
+  this.promoted_koma = new Image();
+  this.promoted_koma.width = 80
+  this.promoted_koma.src = this.promoted_koma_img_path;
+
+  this.promoted = false;
+  this.elm = this.koma;
+};
+Koma.prototype.turnOver = function(){
+  this.promoted = !this.promoted;
+  if(this.promoted){
+    //TODO:komaの位置にpromoted_komaを挿入
+    this.elm = this.promoted_koma;
+  }
+  else{
+    //TODO:promoted_komaの位置にkomaを挿入
+    this.elm = this.koma;
+  }
+  
+};
+
 window.addEventListener("load",function(){
   var grid_path = STATIC_URL + "images/grid.png";
   var board = document.getElementById("shogi");
   board.style.width = "720px";
   board.style.height = "720px";
+
+  var img = new Image();
+  img.src = grid_path;
+  img.width = 700;
+  img.height = 80;
+  board.appendChild(img);
+
   for(var y = 0; y < 9; y++){
     for(var x = 0; x < 9; x++){
       var img = new Image();
@@ -22,6 +73,16 @@ window.addEventListener("load",function(){
       board.appendChild(img);
     }
   }
+  var img = new Image();
+  img.src = grid_path;
+  img.width = 700;
+  img.height = 80;
+  board.appendChild(img);
+ 
+
+  var koma = new Koma("pawn");
+  document.body.appendChild(koma.elm);
+
 
 
   var url = "ws://" + location.host + "/shogisocket";
