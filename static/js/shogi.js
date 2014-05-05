3a9b6cd07242d1cb77cebcc20197fb1155f87f58
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-05 20:54:53 +0900

    Add none drag image

--- static/js/shogi.js
+++ static/js/shogi.js
@@ -53,30 +53,38 @@ Koma.prototype.turnOver = function(){
   
 };
 
+
+function makeImg(path, width, height){
+  var img = new Image();
+  img.src = path;
+  img.className = "nonedrag";
+  if(width){
+    img.width = width;
+  }
+  if(height){
+    img.height = height;
+  }
+  return img;
+}
+
 window.addEventListener("load",function(){
   var grid_path = STATIC_URL + "images/grid.png";
   var board = document.getElementById("shogi");
   board.style.width = "720px";
   board.style.height = "720px";
 
-  var img = new Image();
-  img.src = grid_path;
-  img.width = 700;
-  img.height = 80;
+  var img = makeImg(grid_path,700,80);
+
   board.appendChild(img);
 
   for(var y = 0; y < 9; y++){
     for(var x = 0; x < 9; x++){
-      var img = new Image();
-      img.src = grid_path;
+      var img = makeImg(grid_path);
       img.style.float = "left";
       board.appendChild(img);
     }
   }
-  var img = new Image();
-  img.src = grid_path;
-  img.width = 700;
-  img.height = 80;
+  var img = makeImg(grid_path, 700, 80);
   board.appendChild(img);
  
 
@@ -90,7 +98,6 @@ window.addEventListener("load",function(){
   socket.onmessage = function(event) {
     console.log(JSON.parse(event.data));
   }
-
 
 },true);
 
